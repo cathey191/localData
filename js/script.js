4ac7c18f7ebc530a8a96ee56aa332a5e5c0015a3
Catherine Clague <845870941@bun2-206-17.local>
2018-07-30 10:49:00 +1200

    Json linked with jQuery

--- js/script.js
+++ js/script.js
@@ -41,37 +41,51 @@
 //
 // JSON.parse(variable);
 
-// getting JSON using pure JS
-var xhttp = new XMLHttpRequest();
+// // getting JSON using pure JS
+// var xhttp = new XMLHttpRequest();
+//
+// xhttp.onreadystatechange = function() {
+// 	// readyState
+// 	// 0 - request was not initialized
+// 	// 1 - server connection estabished
+// 	// 2 - request recieved
+// 	// 3 - processing the request
+// 	// 4 - request has finished and the data/response is ready
+// 	//
+// 	// status
+// 	// https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+// 	// 200 - OK
+// 	// 403 - Forbidden / need to log in
+// 	// 404 - Not found
+//
+// 	if (this.status == 403) {
+// 		console.log('Forbidden, cannot access information');
+// 		return;
+// 	} else if (this.status == 404) {
+// 		console.log('Error, file not found');
+// 		return;
+// 	}
+//
+// 	if (this.status == 200 && this.readyState == 4) {
+// 		console.log(this.responseText);
+// 		var data = JSON.parse(this.responseText);
+// 		console.log(data);
+// 	}
+// };
+//
+// xhttp.open('GET', 'data/data.json', true);
+// xhttp.send();
 
-xhttp.onreadystatechange = function() {
-	// readyState
-	// 0 - request was not initialized
-	// 1 - server connection estabished
-	// 2 - request recieved
-	// 3 - processing the request
-	// 4 - request has finished and the data/response is ready
-	//
-	// status
-	// https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-	// 200 - OK
-	// 403 - Forbidden / need to log in
-	// 404 - Not found
-
-	if (this.status == 403) {
-		console.log('Forbidden, cannot access information');
-		return;
-	} else if (this.status == 404) {
-		console.log('Error, file not found');
-		return;
+// getting JSON using jQuery
+$.ajax({
+	type: 'GET',
+	url: 'data/data.json',
+	dataType: 'json',
+	success: function(dataFromJSON) {
+		console.log(dataFromJSON);
+	},
+	error: function(error) {
+		console.log(error);
+		console.log('Something went wrong with the connection');
 	}
-
-	if (this.status == 200 && this.readyState == 4) {
-		console.log(this.responseText);
-		var data = JSON.parse(this.responseText);
-		console.log(data);
-	}
-};
-
-xhttp.open('GET', 'data/data.json', true);
-xhttp.send();
+});
